e948a7201a920f5b21bb40ba60da27d1bbfd5d41
JupiterChild33 <151514461+JupiterChild33@users.noreply.github.com>
2023-11-20 11:31:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GIT_aufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A1.Start();
+            A2.Start();
+        }
+    }
+}
